7160fa1e6866f6b65afbad2228d4aa1bc90319fc
Joee <32849562+Joee-D@users.noreply.github.com>
2023-11-22 22:04:56 +0800

    Add files via upload

--- nftables.sh
+++ nftables.sh
@@ -1,45 +1,30 @@
 #!/bin/bash
 
 INTERFACE='lo'
-TPROXY_PORT=7895 ## 和 sing-box 中定义的一致
+TPROXY_PORT=7896 ## 和 sing-box 中定义的一致
 ROUTING_MARK=666 ## 和 sing-box 中定义的一致
 PROXY_FWMARK=1
 PROXY_ROUTE_TABLE=100
 
-define LOCAL_NETWORK = {
-    100.64.0.0/10,
-    127.0.0.0/8,
-    169.254.0.0/16,
-    172.16.0.0/12,
-    192.0.0.0/24,
-    224.0.0.0/4,
-    240.0.0.0/4,
-    255.255.255.255/32
-} ##添加本地地址规则
-
-define DIRECT = {
-    192.168.1.1-192.168.1.100
-} ##添加直连ip地址规则
-
 clearFirewallRules()
 {
     # ----- 删除路由表 -----
-    IPRULE=$(ip rule show | grep $PROXY_ROUTE_TABLE)
-    if [ -n "$IPRULE" ]
-    then
-        ip -f inet rule del fwmark $PROXY_FWMARK lookup $PROXY_ROUTE_TABLE
-        ip -f inet route del local default dev $INTERFACE table $PROXY_ROUTE_TABLE
-        echo "clear ip rule"
-    fi
+    #IPRULE=$(ip rule show | grep $PROXY_ROUTE_TABLE)
+    #if [ -n "$IPRULE" ]
+    #then
+    #    ip -f inet rule del fwmark $PROXY_FWMARK lookup $PROXY_ROUTE_TABLE
+    #    ip -f inet route del local default dev $INTERFACE table $PROXY_ROUTE_TABLE
+    #    echo "clear ip rule"
+    #fi
     # ----- 删除防火墙规则 -----
     for nft in "mangle_prerouting" "mangle_output"; do
-        local handles=$(nft -a list chain inet fw4 ${nft} 2>/dev/null | grep -E "prerouting_tproxy" | grep -E "output_tproxy" | awk -F '# handle ' '{print$2}')
+        local handles=$(nft -a list chain inet fw4 ${nft} 2>/dev/null | grep -E "_tproxy" | awk -F '# handle ' '{print$2}')
             for handle in $handles; do
             nft delete rule inet fw4 ${nft} handle ${handle} 2>/dev/null
         done
     done
     # ----- 删除防火墙链 -----
-    for handle in $(nft -a list chains | grep -E "chain prerouting_tproxy" | grep -E "chain output_tproxy" | awk -F '# handle ' '{print$2}'); do
+    for handle in $(nft -a list chains | grep -E "_tproxy" | awk -F '# handle ' '{print$2}'); do
         nft delete chain inet fw4 handle ${handle} 2>/dev/null
     done
     # ----- 删除防火墙集合 -----
@@ -52,22 +37,21 @@ clearFirewallRules()
 setFirewallRules()
 {
     # ----- 添加路由表 -----
-    ip -f inet rule add fwmark $PROXY_FWMARK lookup $PROXY_ROUTE_TABLE
-    ip -f inet route add local default dev $INTERFACE table $PROXY_ROUTE_TABLE
+    #ip -f inet rule add fwmark $PROXY_FWMARK lookup $PROXY_ROUTE_TABLE
+    #ip -f inet route add local default dev $INTERFACE table $PROXY_ROUTE_TABLE
 
     # ----- 防火墙ip地址集合 -----
     nft add set inet fw4 localnetwork { type ipv4_addr \; flags interval \; }
-    nft add element inet fw4 localnetwork $LOCAL_NETWORK
+    nft add element inet fw4 localnetwork { 100.64.0.0/10, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.0.0.0/24, 224.0.0.0/4, 192.168.0.0/16 }
     nft add set inet fw4 direct { type ipv4_addr \; flags interval \; }
-    nft add element inet fw4 direct $DIRECT
+    nft add element inet fw4 direct { 192.168.1.1-192.168.1.100 }
 
     # ----- prerouting 局域网设备透明代理 -----
     nft add chain inet fw4 prerouting_tproxy
-    #nft add rule inet fw4 prerouting_tproxy meta l4proto {tcp,udp} th dport 53 tproxy to :$TPROXY_PORT accept ## DNS透明代理
     nft add rule inet fw4 prerouting_tproxy ip daddr @localnetwork return ## 绕过局域网地址
     nft add rule inet fw4 prerouting_tproxy ip saddr @direct return ## 绕过直连地址
     nft add rule inet fw4 prerouting_tproxy meta l4proto {tcp,udp} socket transparent 1 meta mark set $PROXY_FWMARK return ## 绕过已经建立的tproxy连接
-    nft add rule inet fw4 prerouting_tproxy meta l4proto {tcp,udp} tproxy to :$TPROXY_PORT meta mark set $PROXY_FWMARK accept## 其他流量透明代理
+    nft add rule inet fw4 prerouting_tproxy meta l4proto {tcp,udp} tproxy to :$TPROXY_PORT meta mark set $PROXY_FWMARK ## 其他流量透明代理
 
     # ----- output 网关本机透明代理 -----
     nft add chain inet fw4 output_tproxy
@@ -76,17 +60,17 @@ setFirewallRules()
     nft add rule inet fw4 prerouting_tproxy ip daddr @localnetwork return ## 绕过局域网地址
     nft add rule inet fw4 prerouting_tproxy ip saddr @direct return ## 绕过直连地址
     nft add rule inet fw4 output_tproxy meta l4proto {tcp,udp} meta mark set $PROXY_FWMARK ## 其他流量重路由到prerouting
-    
+
+	#nft add rule inet fw4 mangle_prerouting meta nfproto {ipv4} counter jump prerouting_tproxy
+	#nft add rule inet fw4 nat_output meta nfproto {ipv4} counter jump output_tproxy
+
     echo "set nftables"
 }
 
 if [ $1 = 'start' ]
 then
-    ./sing-box -D /etc/sing-box -c /etc/sing-box/config.json run
     setFirewallRules
-
 elif [ $1 = 'stop' ]
 then
     clearFirewallRules
-    ./sing-box stop
 fi
